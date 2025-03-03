43f1161fa744a9a5509d64eac10521281cd3e1e8
Michael Shepanski <mshepanski@nasuni.com>
2025-03-03 14:32:02 -0500

    Aded Custom CSS lines to install script.

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
+# Following two lines are needed for the Custom CSS Loader extension
+sudo chown -R $(whoami) "$(which code)"
+sudo chown -R $(whoami) /usr/share/code
 ln -s ~/Projects/vscode-pksthemes ~/.vscode/extensions/vscode-pksthemes
