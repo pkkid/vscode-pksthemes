9ab082b02faf277f91b7d5883cffaeb1b5fb2023
Michael Shepanski <mshepanski@nasuni.com>
2025-03-03 19:48:03 -0500

    Add favicons

---
+++ makecss.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+import base64
+import subprocess
+from os.path import dirname, abspath
+
+ROOT = f"{dirname(abspath(__file__))}/favicons"
+CSS = "body:has(footer div[aria-label^='{basename}']) a.window-appicon { background-image: url('data:image/gif;base64,{data}') !important; }"  # noqa
+text = ""
+for root, dirs, files in os.walk(ROOT):
+    for file in files:
+        if file.endswith('.gif'):
+            filepath = os.path.join(root, file)
+            basename = os.path.basename(filepath)
+            with open(filepath, 'rb') as handle:
+                data = base64.b64encode(handle.read())
+            css = CSS.replace('{basename}', basename.split('.')[0])
+            css = css.replace('{data}', data.decode('utf8'))
+            text += f'{css}\n'
+            print(f'{css[0:80]}...')
+
+subprocess.run(['xclip', '-selection', 'clipboard'], input=text.encode('utf8'))
+print('\n==> CSS copied to clipboard.\n')
