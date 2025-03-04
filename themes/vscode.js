268bf0f36a0862b391f7caa91447f0556d1b46db
Michael Shepanski <mshepanski@nasuni.com>
2025-03-04 11:56:17 -0500

    Add title to header. Move some code styles from settings to theme.

---
+++ themes/vscode.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var title = null
+  var titlediv = null
+  
+  // Update the Window Title
+  setInterval(function() {
+    var newtitle = document.title.split(' - ')[1]
+    if (newtitle !== title) {
+      if (titlediv === null) {
+        var appicon = document.querySelector('a.window-appicon')
+        titlediv = document.createElement('div')
+        titlediv.id = 'window-title'
+        appicon.parentNode.insertBefore(titlediv, appicon.nextSibling);
+      }
+      titlediv.textContent = newtitle;
+      title = newtitle
+      console.log(`[PK] ${title}`)
+    }
+  }, 1000)
+})
