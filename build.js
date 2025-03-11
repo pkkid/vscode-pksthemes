4bcc1003801a1019904a25455eb71c4338b19f18
Michael Shepanski <mshepanski@nasuni.com>
2025-03-10 23:01:05 -0400

    Seperate window and syntax  themes

---
+++ build.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+// build.js - Iterates through the theme/syntax-*.json files and appends the
+// window-specific settings to the beginning of the file. It places all the
+// resulting files in the dist folder.
+const fs = require('fs')
+const path = require('path')
+const JSON5 = require('json5')
+
+if (require.main === module) {
+  const window = JSON5.parse(fs.readFileSync('src/window.json', 'utf8'))
+  fs.readdirSync('src').forEach((filename) => {
+    if (filename.startsWith('syntax-') && filename.endsWith('.json')) {
+      const syntax = JSON5.parse(fs.readFileSync(path.join('src', filename), 'utf8'))
+      const theme = { ...window, ...syntax }
+      const themename = filename.replace('syntax-', 'theme-')
+      fs.writeFileSync(`themes/${themename}`, JSON.stringify(theme, null, 2))
+    }
+  });
+  console.log('Done!')
+}
